e5519324e896a40edc69ec6e8ad9fe10fb2d2362
chaitanyasharma31 <c.shrma31@gmail.com>
2023-01-18 16:04:19 -0700

    corrected var

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -5,8 +5,8 @@ import com.example.Docker
 def call(String imageName) {
                     echo "building the docker image..."
                    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-                        sh "docker build -t $imagename ."
+                        sh "docker build -t $imageName ."
                         sh "echo $PASS | docker login -u $USER --password-stdin"
-                        sh "docker push $imagename"
+                        sh "docker push $imageName"
     }
 } 
