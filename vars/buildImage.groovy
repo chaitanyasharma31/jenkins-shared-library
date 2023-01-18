ffc99e948e8305f9975164abec15cd4a823fe87f
chaitanyasharma31 <c.shrma31@gmail.com>
2023-01-18 14:43:23 -0700

    modified vars files

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,6 +2,11 @@
 
 import com.example.Docker
 
-def call(String imageName) {
-    return new Docker(this).buildDockerImage(imageName)
-}
+def call() {
+                    echo "building the docker image..."
+                   withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+                        sh 'docker build -t cshrma/demo-app:jma1.1 .'
+                        sh "echo $PASS | docker login -u $USER --password-stdin"
+                        sh 'docker push cshrma/demo-app:jma1.1'
+    }
+} 
