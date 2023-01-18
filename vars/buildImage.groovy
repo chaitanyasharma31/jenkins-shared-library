cfbeb3cb124bb396d51eed423456bdc7b0ae0ce2
Chaitanya <101482748+chaitanyasharma31@users.noreply.github.com>
2023-01-18 16:38:31 -0700

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -4,10 +4,10 @@ import com.example.Docker
 
 def call(String imageName) {
     return new docker(this).buildDockerImage(imageName)
-                //     echo "building the docker image..."
-                //    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-                //         sh "docker build -t $imageName ."
-                //         sh "echo $PASS | docker login -u $USER --password-stdin"
-                //         sh "docker push $imageName"
+                     echo "building the docker image..."
+                    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+                         sh "docker build -t $imageName ."
+                      sh "echo $PASS | docker login -u $USER --password-stdin"
+                         sh "docker push $imageName"
     
 } 
