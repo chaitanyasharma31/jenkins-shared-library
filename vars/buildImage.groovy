cad7277581886fdd4122ce4a076c8cef90c5e003
chaitanyasharma31 <c.shrma31@gmail.com>
2023-01-18 16:18:47 -0700

    used import

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,12 +1,13 @@
 #!/usr/bin/env groovy
 
-// import com.example.Docker
+import com.example.Docker
 
 def call(String imageName) {
-                    echo "building the docker image..."
-                   withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-                        sh "docker build -t $imageName ."
-                        sh "echo $PASS | docker login -u $USER --password-stdin"
-                        sh "docker push $imageName"
-    }
+    return new docker(this).buildDockerImage(imageName)
+                //     echo "building the docker image..."
+                //    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+                //         sh "docker build -t $imageName ."
+                //         sh "echo $PASS | docker login -u $USER --password-stdin"
+                //         sh "docker push $imageName"
+    
 } 
